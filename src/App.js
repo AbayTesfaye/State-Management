2ef974c0c43fb62af53857d969b7f6fbdbe81452
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-22 09:08:13 -0700

    Handle useReducer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
+import { useReducer } from "react";
 import "./App.css";
-import { useState } from "react";
+
+const intialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increament":
+      return state + 1;
+    case "decreament":
+      return state - 1;
+    case "reset":
+      return intialState;
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [list, setList] = useState(["Abay", "Dani", "Asme"]);
-  const [name, setName] = useState();
-
-  function handleInput(e) {
-    setName(e.target.value);
-  }
-
-  function addName() {
-    setList([...list, name]);
-  }
-
+  const [count, Dispatch] = useReducer(reducer, intialState);
   return (
     <div>
-      <h1>List of Student </h1>
-      {list.map((name) => {
-        return (
-          <ul>
-            <li key={name}>{name}</li>
-          </ul>
-        );
-      })}
-      <div>
-        <input type="text" onChange={handleInput} />
-        <button onClick={addName}>Add Name</button>
-      </div>
+      <h1>Count : {count}</h1>
+      <button onClick={() => Dispatch("increament")}>Increment</button>
+      <button onClick={() => Dispatch("decreament")}>Decrement</button>
+      <button onClick={() => Dispatch("reset")}>Reset</button>
     </div>
   );
 }
