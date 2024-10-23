0f961b103655e0b51d1eb5177cc671405edd20b0
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-23 15:05:35 -0700

    Fetch data when button is clicked using useEffect

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import axios from "axios";
 function App() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [buttonIdClicked, setButtonIdClicked] = useState(1);
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${buttonIdClicked}`)
       .then((response) => setPost(response.data))
       .catch((error) => console.error("Error occurred: ", error));
-  }, [id]); // Dependency array added to prevent continuous API calls
+  }, [buttonIdClicked]); // Dependency array added to prevent continuous API calls
 
+  const handleClick = () => {
+    setButtonIdClicked(id);
+  };
   return (
     <div>
       <input value={id} onChange={(e) => setId(e.target.value)} />
+      <button onClick={() => handleClick()}>Fetch</button>
       <h1>{post.title}</h1>
     </div>
   );
